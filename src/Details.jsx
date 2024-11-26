a65dd4fc50dd33b70b5be752664213c58252b39b
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-11-26 10:54:55 +0100

    add modal

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Modal from "./Modal.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 import fetchPet from "./FetchPet.jsx";
 import Carousel from "./Carousel.jsx";
 
 const Details = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
   const results = useQuery(["details", id], fetchPet);
 
   if (results.isLoading) {
@@ -24,8 +27,19 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Are you sure you want to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
